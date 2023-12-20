18ffe2d904279fe4e26d4c661e50d31afba94299
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-21 00:05:54 +0530

    feat: add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import { NavigationBar } from "./layouts/NavigationBar";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">
-      Hello world!
-    </h1>
-  );
+  return <NavigationBar />;
 }
 
 export default App;
