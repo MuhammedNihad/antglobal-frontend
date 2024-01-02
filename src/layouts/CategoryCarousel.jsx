08b497b583f3e43b6452efaa5b80c4b57408731e
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-02 22:10:44 +0530

    style: set default spaceBetween parameter of category carousel as 5px

--- src/layouts/CategoryCarousel.jsx
+++ src/layouts/CategoryCarousel.jsx
@@ -102,37 +102,32 @@ export const CategoryCarousel = () => {
       <Swiper
         // Default parameters
         slidesPerView={1}
+        spaceBetween={5}
         // Responsive breakpoints
         breakpoints={{
           // when window width is >= 320px
           320: {
             slidesPerView: 1,
-            // spaceBetween: 20,
           },
           // when window width is >= 480px
           480: {
             slidesPerView: 2,
-            // spaceBetween: 30,
           },
           // when window width is >= 640px
           640: {
             slidesPerView: 3,
-            // spaceBetween: 20,
           },
           // when window width is >= 768px
           768: {
             slidesPerView: 4,
-            // spaceBetween: 40,
           },
           // when window width is >= 1024px
           1024: {
             slidesPerView: 5,
-            // spaceBetween: 50,
           },
           // when window width is >= 1280px
           1280: {
             slidesPerView: 6,
-            // spaceBetween: 10,
           },
           // when window width is >= 1536px
           1536: {
