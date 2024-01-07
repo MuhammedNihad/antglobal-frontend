9dae7ab9ca1bcc67ba3d702e818a99ecaaeb517c
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-07 18:29:11 +0530

    style: add colors to icons in product card on hover

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,7 +10,7 @@ import { ArrowUpRight, WhatsappLogo } from "@phosphor-icons/react";
  */
 export const ProductCard = ({ name, image, altText }) => {
   return (
-    <Card isFooterBlurred className="rounded-3xl border-none">
+    <Card isFooterBlurred className="group rounded-3xl border-none">
       <Image
         alt={altText}
         className="relative object-cover"
@@ -18,8 +18,12 @@ export const ProductCard = ({ name, image, altText }) => {
         src={image}
         width={250}
       />
-      <div className="absolute right-2 top-2 z-10 inline-flex items-center rounded-3xl bg-white p-2 shadow-md">
-        <ArrowUpRight size={20} alt="Arrow up icon on product image" />
+      <div className="absolute right-2 top-2 z-10 inline-flex items-center rounded-3xl bg-white p-2 shadow-md group-hover:bg-accent-950">
+        <ArrowUpRight
+          size={20}
+          alt="Arrow up icon on product image"
+          className="text-dark group-hover:text-white"
+        />
       </div>
       <CardFooter className="inset-x-1px absolute bottom-1 z-10 ml-1 w-[calc(100%_-_8px)] justify-between overflow-hidden rounded-3xl border-1 border-white/20 py-1 shadow-small before:rounded-xl before:bg-white/10">
         <p className="line-clamp-2 text-base font-semibold text-white/80 lg:text-sm">
