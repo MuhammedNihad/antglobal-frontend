9e549f0cf3ef0eb3f624bff5d614e733c30a1288
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-07 14:49:20 +0530

    feat(style): add arrow up right icon on product image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Card, CardFooter, Image, Button } from "@nextui-org/react";
-import { WhatsappLogo } from "@phosphor-icons/react";
+import { ArrowUpRight, WhatsappLogo } from "@phosphor-icons/react";
 
 /**
  * Render a product card component.
@@ -13,11 +13,14 @@ export const ProductCard = ({ name, image, altText }) => {
     <Card isFooterBlurred className="rounded-3xl border-none">
       <Image
         alt={altText}
-        className="object-cover"
+        className="relative object-cover"
         height={250}
         src={image}
         width={250}
       />
+      <div className="absolute right-2 top-2 z-10 inline-flex items-center rounded-3xl bg-white p-2 shadow-md">
+        <ArrowUpRight size={20} alt="Arrow up icon on product image" />
+      </div>
       <CardFooter className="inset-x-1px absolute bottom-1 z-10 ml-1 w-[calc(100%_-_8px)] justify-between overflow-hidden rounded-3xl border-1 border-white/20 py-1 shadow-small before:rounded-xl before:bg-white/10">
         <p className="line-clamp-2 text-base font-semibold text-white/80 lg:text-sm">
           {name}
