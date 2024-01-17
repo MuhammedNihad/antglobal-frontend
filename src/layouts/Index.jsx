227055aef1f03e0bb91ea5c3ead65e1be1fb0fbd
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-17 23:25:06 +0530

    chore: integrate `react-router-dom` with `NextUI` router
    Integrates NextUI with react-router-dom by passing useNavigate hook as props to NextUIProvider
    of NextUI. So that we can simply pass routes in href props to many NextUI components.

--- src/layouts/Index.jsx
+++ src/layouts/Index.jsx
@@ -1,15 +1,16 @@
-import { ScrollRestoration, Outlet } from "react-router-dom";
-
+import { ScrollRestoration, Outlet, useNavigate } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
 import { NavigationBar } from "./NavigationBar";
 import { Footer } from "./Footer";
 
 export const Layout = () => {
+  const navigate = useNavigate();
   return (
-    <div className="bg-primary-50 p-3 lg:px-10">
+    <NextUIProvider navigate={navigate} className="bg-primary-50 p-3 lg:px-10">
       <ScrollRestoration />
       <NavigationBar />
       <Outlet />
       <Footer />
-    </div>
+    </NextUIProvider>
   );
 };
