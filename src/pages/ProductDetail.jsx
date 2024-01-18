d242f03022eec741f430f12619bcc4e6f2d47af7
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-18 15:56:36 +0530

    refactor: use Image component of NextUI in product image gallery

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// import swiper required modules
 import { FreeMode, Navigation, Thumbs, Zoom } from "swiper/modules";
+import { Image } from "@nextui-org/react";
 
 export const ProductDetail = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  const productImages = [
+    {
+      src: "https://placehold.co/500x500/17276e/white/png",
+    },
+    {
+      src: "https://placehold.co/500x500/f67860/white/png",
+    },
+    {
+      src: "https://placehold.co/500x500/660012/white/png",
+    },
+    {
+      src: "https://placehold.co/500x500/00c895/white/png",
+    },
+    {
+      src: "https://placehold.co/500x500/7e84ff/white/png",
+    },
+  ];
 
   return (
     <section className="mb-3 rounded-3xl bg-secondary-300 p-5 lg:px-10">
@@ -23,46 +40,17 @@ export const ProductDetail = () => {
             modules={[FreeMode, Navigation, Thumbs, Zoom]}
             className="w-72 rounded-3xl sm:w-80 md:w-96 lg:w-[28rem]"
           >
-            <SwiperSlide className="rounded-3xl">
-              <div className="swiper-zoom-container">
-                <img
-                  className=" rounded-3xl"
-                  src="https://placehold.co/500x500/17276e/white/png"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="rounded-3xl">
-              <div className="swiper-zoom-container">
-                <img
-                  className=" rounded-3xl"
-                  src="https://placehold.co/500x500/f67860/white/png"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="rounded-3xl">
-              <div className="swiper-zoom-container">
-                <img
-                  className=" rounded-3xl"
-                  src="https://placehold.co/500x500/660012/white/png"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="rounded-3xl">
-              <div className="swiper-zoom-container">
-                <img
-                  className=" rounded-3xl"
-                  src="https://placehold.co/500x500/00c895/white/png"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="rounded-3xl">
-              <div className="swiper-zoom-container">
-                <img
-                  className=" rounded-3xl"
-                  src="https://placehold.co/500x500/7e84ff/white/png"
-                />
-              </div>
-            </SwiperSlide>
+            {productImages.map((item, index) => (
+              <SwiperSlide className="rounded-3xl" key={index}>
+                <div className="swiper-zoom-container">
+                  <Image
+                    className="rounded-3xl"
+                    src={item.src}
+                    loading="eager"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
           <Swiper
             onSwiper={setThumbsSwiper}
@@ -80,36 +68,11 @@ export const ProductDetail = () => {
             modules={[FreeMode, Navigation, Thumbs]}
             className="w-72 rounded-2xl sm:w-80 md:w-96 md:rounded-3xl lg:w-[28rem]"
           >
-            <SwiperSlide className="rounded-2xl md:rounded-3xl">
-              <img
-                className="rounded-2xl md:rounded-3xl"
-                src="https://placehold.co/500x500/17276e/white/png"
-              />
-            </SwiperSlide>
-            <SwiperSlide className="rounded-3xl sm:rounded-2xl">
-              <img
-                className="rounded-2xl md:rounded-3xl"
-                src="https://placehold.co/500x500/f67860/white/png"
-              />
-            </SwiperSlide>
-            <SwiperSlide className="rounded-2xl md:rounded-3xl">
-              <img
-                className="rounded-2xl md:rounded-3xl"
-                src="https://placehold.co/500x500/660012/white/png"
-              />
-            </SwiperSlide>
-            <SwiperSlide className="rounded-2xl md:rounded-3xl">
-              <img
-                className="rounded-2xl md:rounded-3xl"
-                src="https://placehold.co/500x500/00c895/white/png"
-              />
-            </SwiperSlide>
-            <SwiperSlide className="rounded-2xl md:rounded-3xl">
-              <img
-                className="rounded-2xl md:rounded-3xl"
-                src="https://placehold.co/500x500/7e84ff/white/png"
-              />
-            </SwiperSlide>
+            {productImages.map((item, index) => (
+              <SwiperSlide className="rounded-2xl md:rounded-3xl" key={index}>
+                <Image className="rounded-2xl md:rounded-3xl" src={item.src} />
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
