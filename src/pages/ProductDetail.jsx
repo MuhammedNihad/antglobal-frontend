87eac79978e2c45de7fe730937e0e807f02719a6
Muhammed Nihad <muhammednihad999@gmail.com>
2024-01-18 12:34:19 +0530

    feat: add zoom feature to product image gallery

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // import swiper required modules
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { FreeMode, Navigation, Thumbs, Zoom } from "swiper/modules";
 
 export const ProductDetail = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -12,6 +12,7 @@ export const ProductDetail = () => {
       <div className="flex flex-row flex-wrap justify-center gap-12 md:gap-20 lg:gap-28">
         <div className="flex flex-col gap-[5px]">
           <Swiper
+            zoom
             loop
             navigation
             spaceBetween={5}
@@ -19,38 +20,48 @@ export const ProductDetail = () => {
               swiper:
                 thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
             }}
-            modules={[FreeMode, Navigation, Thumbs]}
+            modules={[FreeMode, Navigation, Thumbs, Zoom]}
             className="w-72 rounded-3xl sm:w-80 md:w-96 lg:w-[28rem]"
           >
             <SwiperSlide className="rounded-3xl">
-              <img
-                className=" rounded-3xl"
-                src="https://placehold.co/500x500/17276e/white/png"
-              />
+              <div className="swiper-zoom-container">
+                <img
+                  className=" rounded-3xl"
+                  src="https://placehold.co/500x500/17276e/white/png"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide className="rounded-3xl">
-              <img
-                className=" rounded-3xl"
-                src="https://placehold.co/500x500/f67860/white/png"
-              />
+              <div className="swiper-zoom-container">
+                <img
+                  className=" rounded-3xl"
+                  src="https://placehold.co/500x500/f67860/white/png"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide className="rounded-3xl">
-              <img
-                className=" rounded-3xl"
-                src="https://placehold.co/500x500/660012/white/png"
-              />
+              <div className="swiper-zoom-container">
+                <img
+                  className=" rounded-3xl"
+                  src="https://placehold.co/500x500/660012/white/png"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide className="rounded-3xl">
-              <img
-                className=" rounded-3xl"
-                src="https://placehold.co/500x500/00c895/white/png"
-              />
+              <div className="swiper-zoom-container">
+                <img
+                  className=" rounded-3xl"
+                  src="https://placehold.co/500x500/00c895/white/png"
+                />
+              </div>
             </SwiperSlide>
             <SwiperSlide className="rounded-3xl">
-              <img
-                className=" rounded-3xl"
-                src="https://placehold.co/500x500/7e84ff/white/png"
-              />
+              <div className="swiper-zoom-container">
+                <img
+                  className=" rounded-3xl"
+                  src="https://placehold.co/500x500/7e84ff/white/png"
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
           <Swiper
